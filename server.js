b8f7a4cc40935a6600510a77697cf8ca9d6914c2
jbluft <jbluft@gmail.com>
2018-03-10 09:46:18 -0500

    css fix

--- server.js
+++ server.js
@@ -30,18 +30,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
 });
-
-
-// Set Handlebars.
-// var exphbs = require("express-handlebars");
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
-// // Import routes and give the server access to them.
-// var routes = require("./controllers/catsController.js");
-
-// app.use(routes);
 
