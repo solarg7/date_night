75542ed836eb4f0ebae2ccd686f1e4b7f37e3203
jbluft <jbluft@gmail.com>
2018-03-10 10:01:53 -0500

    compile

--- server.js
+++ server.js
@@ -21,8 +21,6 @@ app.get('/post', function(req,res){
   res.render('layouts/post');
 });
 
-// // Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static("public"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,18 +28,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+
+app.use(express.static(__dirname + '/public'));
+
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
 });
-
-
-// Set Handlebars.
-// var exphbs = require("express-handlebars");
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
-// // Import routes and give the server access to them.
-// var routes = require("./controllers/catsController.js");
-
-// app.use(routes);
-
